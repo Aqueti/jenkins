0212a325d69271f74290ce0938f022e71b162683
alxstep <al.stepenko@gmail.com>
2018-02-24 09:10:49 -0500

    Update install.sh

--- pipeline/bash/run_scr/install.sh
+++ pipeline/bash/run_scr/install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-plname=`echo $1 | cut -d'/' -f 1`
-bname=`echo $1 | cut -d'/' -f 2`
-last=`find $HOME/deb/$plname/branches/$2/builds -type d -regex "./[0-9]+" | sed "s/^..//" | sort -n | tail -1`
-dir="$HOME/deb/$1/branches/$2/builds/$last"
+dir="$HOME/deb_files"
+dir2="$HOME/bin_files"
+
+if ! [ -d $dir ]; then
+  mkdir -p $dir
+fi
+
+if ! [ -d $dir2 ]; then
+  mkdir -p $dir2
+fi
+
+rm $dir/* 2>/dev/null
+cp ../../../../*.deb $dir
+cp ../../../testScripts/apitest/bin/* $dir2
 
 sudo dpkg -i $dir/mantisapi*.deb
 sudo dpkg -i $dir/mantis_app*.deb
