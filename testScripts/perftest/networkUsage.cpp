023f1c33afddce4b9ebcedacb37e2253c0939830
Alex Stepenko <al.stepenko@gmail.com>
2018-02-28 08:13:44 -0500

    network usage by V2

--- testScripts/perftest/networkUsage.cpp
+++ testScripts/perftest/networkUsage.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <cstring>
+#include <string>
 #include <map>
 #include <vector>
 #include <list>
 #include <iterator>
+#include <iostream>
 
 #include "mantis/MantisAPI.h"
 #include "gtest/gtest.h"
@@ -28,13 +29,24 @@ void mcamFrameCallback(FRAME frame, void* data)
 
 class TestParams {
   public:
+    struct RT {
+        uint32_t receive;
+        uint32_t transmit;
+    };
+    struct Result {
+        size_t size;
+        RT rt_f;
+        RT rt_s;    
+    };
+
     TestParams() {
         ip = "192.168.10.";
         port = 9999;
         r_port = 11001;     
         duration = 10;
         num_of_mcams = 0;
-        
+        interface = "enp5s0f1";
+         
         bitrateModes.push_back("High");
         bitrateModes.push_back("Medium");
         bitrateModes.push_back("Low");
@@ -54,9 +66,10 @@ class TestParams {
     uint16_t duration;
     uint32_t num_of_mcams;
     
+    string interface;
     vector<MICRO_CAMERA> mcamList;
     AtlCompressionParameters cp;
-    map<string, size_t> res;
+    map<string, Result> res;
     list<FRAME> frames;
 
     vector<string> bitrateModes;
@@ -64,14 +77,12 @@ class TestParams {
     map<string, int> bitrateMapH265;
 };
 
-class NetworkUsage : public TestParams, public ::testing::TestWithParam<int> {
+class NetworkUsage : public TestParams, public ::testing::TestWithParam<int> {    
   protected:
     virtual void SetUp() {
         ostringstream ss;
         ss << ip << GetParam();
         ip = ss.str();
-
-        cout << "ip: " << ip << endl;
 
         mCamConnect(ip.c_str(), port);
 
@@ -102,6 +113,37 @@ class NetworkUsage : public TestParams, public ::testing::TestWithParam<int> {
         mCamDisconnect(ip.c_str(), port);
     }
 
+    string exec(string cmd) {
+      FILE *popen_result;
+      char buff[512];
+      popen_result = popen(cmd.c_str(), "r");
+
+      ostringstream ss;
+
+        if (popen_result) {
+            while (fgets(buff, sizeof(buff), popen_result) != NULL) {
+                ss << buff;
+            }
+        }
+
+      pclose(popen_result);
+
+      return ss.str();
+    }
+
+    RT getRT(string interface) {
+        string cmd;
+        RT rt;
+
+        cmd = "cat /proc/net/dev | grep " + interface + " | awk '{print$2}'";
+        rt.receive = atoi(exec(cmd).c_str());
+
+        cmd = "cat /proc/net/dev | grep " + interface + " | awk '{print$10}'";
+        rt.transmit = atoi(exec(cmd).c_str());
+
+        return rt;
+    }
+
     double getAvgSize() {
         double size = 0;
 
@@ -126,34 +168,43 @@ class NetworkUsage : public TestParams, public ::testing::TestWithParam<int> {
     }
 
     void print_result() {
-        for (map<string, size_t>::iterator it = res.begin(); it != res.end(); it++)
+        cout << "ip: " << ip << endl << endl;
+
+        cout << "\t" << "frames_size\t" << "RX\t\t" << "TX" << endl;
+        for (map<string, Result>::iterator it = res.begin(); it != res.end(); it++)
         {
-            cout << it->first << "\t" << it->second << endl;
+            cout << it->first << "\t" 
+                 << it->second.size << "\t" 
+                 << it->second.rt_s.receive - it->second.rt_f.receive << "\t" 
+                 << it->second.rt_s.transmit - it->second.rt_f.transmit << "\t" 
+                 << endl;
         }
         cout << endl;
     }
 };
 
 INSTANTIATE_TEST_CASE_P(Test, NetworkUsage,
-                        ::testing::Range(6, 10));
+                        ::testing::Range(6, 11));
 
 TEST_P(NetworkUsage, test) {
     for (int i = 0; i < bitrateModes.size(); i++) { 
         for (int j = 0; j < num_of_mcams; j++) {
-            cp = getMCamCompressionParameters( mcamList[i] );
-          
+            cp = getMCamCompressionParameters( mcamList[j] );
+
             if(cp.type == ATL_COMPRESSION_TYPE_H264) {
                 cp.target_bitrate = bitrateMapH264[bitrateModes[i]];
             } else if(cp.type == ATL_COMPRESSION_TYPE_H265 ) {
                 cp.target_bitrate = bitrateMapH265[bitrateModes[i]];
             }
 
-            setMCamCompressionParameters( mcamList[i], cp);
+            setMCamCompressionParameters( mcamList[j], cp);
         }
 
         frames.clear();
+        res[bitrateModes[i]].rt_f = getRT(interface);
         sleep(duration);
-        res[bitrateModes[i]] = getSize();
+        res[bitrateModes[i]].rt_s = getRT(interface);
+        res[bitrateModes[i]].size = getSize();
     }
 
     print_result();
