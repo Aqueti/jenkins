f9c5a98cb24da68adc71496853cf5b7bede607e1
alxstep <al.stepenko@gmail.com>
2018-03-13 16:56:23 -0400

    Update test.py

--- testScripts/webapptest/test.py
+++ testScripts/webapptest/test.py
@@ -14,19 +14,19 @@ class Test(BaseTest):
         self.assertEqual(len(comps), 2)
 
         for comp in comps:
-            self.assertIn("aqt:Camera:", comp.get_attribute("innerText"))
+            self.assertIn("aqt:Camera:", comp.text)
 
         comps[0].click()
 
-        self.assertEqual(aap_sc.prop_status.get_attribute('innerText'), "online")
-        self.assertEqual(aap_sc.prop_recording.get_attribute('innerText'), "false")
-        self.assertEqual(aap_sc.prop_serial.get_attribute('innerText'), "undefined")
-        self.assertEqual(aap_sc.prop_software.get_attribute('innerText'), "undefined")
-        self.assertEqual(aap_sc.prop_kernel.get_attribute('innerText'), "undefined")
-        self.assertEqual(aap_sc.prop_host.get_attribute('innerText'), "undefined")
+        self.assertEqual(aap_sc.prop_status.text, "online")
+        self.assertEqual(aap_sc.prop_recording.text, "false")
+        self.assertEqual(aap_sc.prop_serial.text, "undefined")
+        self.assertEqual(aap_sc.prop_software.text, "undefined")
+        self.assertEqual(aap_sc.prop_kernel.text, "undefined")
+        self.assertEqual(aap_sc.prop_host.text, "undefined")
 
-        self.assertEqual(aap_sc.prop_sensor_module.get_attribute('innerText'), "imx274")
-        self.assertEqual(aap_sc.prop_sensor_host.get_attribute('innerText'), "87878")
+        self.assertEqual(aap_sc.prop_sensor_module.text, "imx274")
+        self.assertEqual(aap_sc.prop_sensor_host.text, "87878")
 
     @unittest.SkipTest
     def test_1(self):
@@ -37,15 +37,15 @@ class Test(BaseTest):
         self.assertEqual(len(comps), 2)
 
         for comp in comps:
-            self.assertIn("aqt:Storage:", comp.get_attribute("innerText"))
+            self.assertIn("aqt:Storage:", comp.text)
 
         comps[0].click()
 
-        self.assertEqual(aap_ss.prop_status.get_attribute('innerText'), "online")
-        self.assertEqual(aap_ss.prop_serial.get_attribute('innerText'), "aqt:Storage:1")
-        self.assertEqual(aap_ss.prop_software.get_attribute('innerText'), "undefined")
-        self.assertEqual(aap_ss.prop_kernel.get_attribute('innerText'), "undefined")
-        self.assertEqual(aap_ss.prop_host.get_attribute('innerText'), "undefined")
+        self.assertEqual(aap_ss.prop_status.text, "online")
+        self.assertEqual(aap_ss.prop_serial.text, "aqt:Storage:1")
+        self.assertEqual(aap_ss.prop_software.text, "undefined")
+        self.assertEqual(aap_ss.prop_kernel.text, "undefined")
+        self.assertEqual(aap_ss.prop_host.text, "undefined")
 
     @unittest.SkipTest
     def test_2(self):
@@ -55,10 +55,10 @@ class Test(BaseTest):
 
         self.assertIsNone(comps)
 
-        self.assertEqual(aap_sr.prop_serial.get_attribute("innerText"), "")
-        self.assertEqual(aap_sr.prop_software.get_attribute("innerText"), "undefined")
-        self.assertEqual(aap_sr.prop_kernel.get_attribute("innerText"), "undefined")
-        self.assertEqual(aap_sr.prop_host.get_attribute("innerText"), "undefined")
+        self.assertEqual(aap_sr.prop_serial.text, "")
+        self.assertEqual(aap_sr.prop_software.text, "undefined")
+        self.assertEqual(aap_sr.prop_kernel.text, "undefined")
+        self.assertEqual(aap_sr.prop_host.text, "undefined")
 
 
 if __name__ == "__main__":
