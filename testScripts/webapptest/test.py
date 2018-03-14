e5914ff1cdeaac64155b27202ee4d6dd970d4838
alxstep <al.stepenko@gmail.com>
2018-03-14 16:49:03 -0400

    Update test.py

--- testScripts/webapptest/test.py
+++ testScripts/webapptest/test.py
@@ -6,10 +6,14 @@ from AquetiAdminPage import *
 class Test(BaseTest):
     browser = "chrome"
 
+    @unittest.SkipTest
     def test_0(self):
         aap_sc = AquetiAdminPageStatusCamera(self.driver)
         self.navigate_to(aap_sc.page_url)
         comps = aap_sc.components
+
+        print(aap_sc.system_current_time.text)
+        print(aap_sc.system_current_date.text)
 
         self.assertEqual(len(comps), 2)
 
@@ -20,12 +24,12 @@ class Test(BaseTest):
 
         self.assertEqual(aap_sc.prop_status.text, "online")
         self.assertEqual(aap_sc.prop_recording.text, "false")
-        self.assertEqual(aap_sc.prop_serial.text, "undefined")
+        self.assertEqual(aap_sc.prop_serialid.text, "undefined")
         self.assertEqual(aap_sc.prop_software.text, "undefined")
         self.assertEqual(aap_sc.prop_kernel.text, "undefined")
         self.assertEqual(aap_sc.prop_host.text, "undefined")
 
-        self.assertEqual(aap_sc.prop_sensor_module.text, "imx274")
+        self.assertEqual(aap_sc.prop_sensor_model.text, "imx274")
         self.assertEqual(aap_sc.prop_sensor_host.text, "87878")
 
     @unittest.SkipTest
@@ -42,7 +46,7 @@ class Test(BaseTest):
         comps[0].click()
 
         self.assertEqual(aap_ss.prop_status.text, "online")
-        self.assertEqual(aap_ss.prop_serial.text, "aqt:Storage:1")
+        self.assertEqual(aap_ss.prop_serialid.text, "aqt:Storage:1")
         self.assertEqual(aap_ss.prop_software.text, "undefined")
         self.assertEqual(aap_ss.prop_kernel.text, "undefined")
         self.assertEqual(aap_ss.prop_host.text, "undefined")
@@ -55,10 +59,18 @@ class Test(BaseTest):
 
         self.assertIsNone(comps)
 
-        self.assertEqual(aap_sr.prop_serial.text, "")
+        self.assertEqual(aap_sr.prop_serialid.text, "")
         self.assertEqual(aap_sr.prop_software.text, "undefined")
         self.assertEqual(aap_sr.prop_kernel.text, "undefined")
         self.assertEqual(aap_sr.prop_host.text, "undefined")
+
+    def test_11(self):
+        aap_i = AquetiAdminPageIssue(self.driver)
+        self.navigate_to(aap_i.page_url)
+
+        aap_sc = aap_i.submit_issue("title", "summary", "description")
+
+        self.assertEqual(aap_sc.page_url, aap_sc.cur_page_url)
 
 
 if __name__ == "__main__":
