cb7c96f6561681bcffbf52005172bd5522e273cf
alxstep <al.stepenko@gmail.com>
2018-03-19 14:40:28 -0400

    Update BaseTest.py

--- testScripts/webapptest/BaseTest.py
+++ testScripts/webapptest/BaseTest.py
@@ -1,31 +1,25 @@
 from selenium.webdriver import ChromeOptions
 from selenium import webdriver
 import unittest
+import datetime
 
 
 class BaseTest(unittest.TestCase):
     driver = None
     browser = "chrome"
 
-    _cnt = 0
-
     @property
     def screenshot_path(self):
-        self._cnt = self._cnt + 1
-        return '/home/astepenko/Pictures/tests/' + self.script_name + "_" + str(self._cnt) + ".png"
+        return '/home/astepenko/Pictures/tests/' + self.script_name + "_" + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + ".png"
 
-    def __call__(self, *args, **kwargs):
-        self.script_name = self.__class__.__name__ + "_" + self.__dict__['_testMethodName']
-        self.log_path = '/home/astepenko/Pictures/tests/' + self.script_name + ".txt"
-
-        super(BaseTest, self).__call__(*args, **kwargs)
+    #def __call__(self, *args, **kwargs):
+    #    super(BaseTest, self).__call__(*args, **kwargs)
 
     def setUp(self):
         if self.browser == "chrome":
-            #opts = ChromeOptions()
-            #opts.add_experimental_option("detach", True)
-            #self.driver = webdriver.Chrome(chrome_options=opts)
-            self.driver = webdriver.Chrome()
+            opts = ChromeOptions()
+            opts.add_experimental_option("detach", True)
+            self.driver = webdriver.Chrome(chrome_options=opts)
         elif self.browser == "ff":
             self.driver = webdriver.Firefox()
         else:
@@ -33,7 +27,26 @@ class BaseTest(unittest.TestCase):
 
         self.driver.maximize_window()
 
+        self.script_name = self.__class__.__name__ + "_" + self.__dict__['_testMethodName']
+
+        self.start_time = datetime.datetime.now()
+
+        self.log_path = '/home/astepenko/Pictures/tests/' + self.script_name + "_" + self.start_time.strftime('%Y-%m-%d_%H:%M:%S') + ".txt"
+
+        self.add_to_log("Suite:\t" + self.__class__.__name__ + "\n" +
+                        "Test:\t" + self.__dict__['_testMethodName'] + "\n" +
+                        "Date:\t" + self.start_time.strftime('%Y-%m-%d %H:%M:%S') + "\n\n")
+
     def tearDown(self):
+        self.end_time = datetime.datetime.now()
+
+        self.result = "PASSED"
+
+        self.add_to_log("\n-----------------------------\n" +
+                        "Result: " + self.result + "\n"
+                        "Exec time: " +
+                        str(datetime.timedelta(seconds=int((self.end_time - self.start_time).total_seconds()))))
+
         self.driver.close()
 
     def navigate_to(self, url=""):
@@ -46,3 +59,7 @@ class BaseTest(unittest.TestCase):
                 self.driver.get(self.page_url)
             else:
                 self.driver.get(url)
+
+    def add_to_log(self, text):
+        with open(self.log_path, "a") as log:
+            log.write(text)
