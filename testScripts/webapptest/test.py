06069d5a3090be617f5e69bfd9a8f76c80378f17
alxstep <al.stepenko@gmail.com>
2018-03-21 16:50:19 -0400

    Update test.py

--- testScripts/webapptest/test.py
+++ testScripts/webapptest/test.py
@@ -73,7 +73,8 @@ class Test(BaseTest):
         aap_sc = aap_i.submit_issue("title", "summary", "description")
 
         self.assertEqual(aap_sc.page_url, aap_sc.cur_page_url)
-
+    
+    @unittest.SkipTest
     def test_12(self):
         aap_cc = AquetiAdminPageConfigurationCamera(self.driver)
         self.navigate_to(aap_cc.page_url)
@@ -96,7 +97,61 @@ class Test(BaseTest):
         aap_cc._(aap_cc.night_mode_chkb)
         aap_cc._(aap_cc.transport_mode_dd, "10 bit")
         aap_cc._(aap_cc.framerate_dd, "30 fps")
+    
+    @unittest.SkipTest
+    def test_sc_move(self):
+        aap_cc = AquetiAdminPageConfigurationCameraImage(self)
+        self.navigate_to(aap_cc.page_url)
+
+        aap_cc.click_links()
+
+        aap_cc = AquetiAdminPageConfigurationCameraImage(self)
+        self.navigate_to(aap_cc.page_url)
+
+        vs = dict()
+        vs['before'] = [self.vs.PanDegrees(), self.vs.TiltDegrees(), self.vs.HorizontalFOVDegrees(), self.vs.VerticalFOVDegrees()]
+        aap_cc._(aap_cc.arrow_down_btn)
+        vs['up'] = vs['before'] = [self.vs.PanDegrees(), self.vs.TiltDegrees(), self.vs.HorizontalFOVDegrees(), self.vs.VerticalFOVDegrees()]
+        aap_cc._(aap_cc.arrow_down_btn)
+        vs['down'] = vs['before'] = [self.vs.PanDegrees(), self.vs.TiltDegrees(), self.vs.HorizontalFOVDegrees(), self.vs.VerticalFOVDegrees()]
+        aap_cc._(aap_cc.arrow_left_btn)
+        vs['left'] = vs['before'] = [self.vs.PanDegrees(), self.vs.TiltDegrees(), self.vs.HorizontalFOVDegrees(), self.vs.VerticalFOVDegrees()]
+        aap_cc._(aap_cc.arrow_right_btn)
+        vs['right'] = vs['before'] = [self.vs.PanDegrees(), self.vs.TiltDegrees(), self.vs.HorizontalFOVDegrees(), self.vs.VerticalFOVDegrees()]
+
+        print("{0}\n{1}\n{2}\n{3}\n{4}".format(vs['before'], vs['up'], vs['down'], vs['left'], vs['right']))
+
+        self.assertNotEquals(vs['before'], vs['up'])
+        self.assertNotEquals(vs['up'], vs['down'])
+        self.assertNotEquals(vs['down'], vs['left'])
+        self.assertNotEquals(vs['left'], vs['right'])
+        self.assertNotEquals(vs['right'], vs['before'])
+
+    @unittest.SkipTest
+    def test_sc_zoom(self):
+        aap_cc = AquetiAdminPageConfigurationCameraImage(self)
+        self.navigate_to(aap_cc.page_url)
+
+        aap_cc.click_links()
+
+        aap_cc = AquetiAdminPageConfigurationCameraImage(self)
+        self.navigate_to(aap_cc.page_url)
+
+        vs = dict()
+        vs['before'] = self.vs.Zoom()
+        self.vs.Zoom(self.vs.Zoom() * 2)
+        vs['in'] = self.vs.Zoom()
+        self.vs.Zoom(self.vs.Zoom() * 0.6)
+        vs['out'] = self.vs.Zoom()
+
+        print("{0}\n{1}\n{2}".format(vs['before'], vs['in'], vs['out']))
+
+        self.assertEquals(vs['before'] * 2, vs['in'])
+        self.assertEquals(vs['in'] * 0.6, vs['out'])
 
 
 if __name__ == "__main__":
-    unittest.main()
+    #unittest.main()
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(APITest)
+    unittest.TextTestRunner().run(suite)
