eb326424a4f6a1ad4f96df589a253554e4aa2cff
alxstep <al.stepenko@gmail.com>
2018-08-22 15:36:13 -0400

    Update genMainPage.sh

--- pipeline_new/bash/genMainPage.sh
+++ pipeline_new/bash/genMainPage.sh
@@ -8,7 +8,7 @@ DEV_FILE="$WEBHOME/branches.html"
 S3_DEST="s3://aqueti.operations/repositories"
 BRANCH=$1
 
-if [ $BRANCH == "slider" ]; then
+if [ $BRANCH == "master" ]; then
    echo "Removing old documentation at $DOC_HOME"
    rm -r $DOC_HOME/*
    mkdir -p $DOC_HOME
