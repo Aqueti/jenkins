e89abab79fc116cd571717deda1f39f6b16fb872
Blake <blakerobinett@gmail.com>
2019-01-15 11:27:25 -0500

    updated qws debian package to set up service

--- qwsdeb/makeQWebServerDeb.sh
+++ qwsdeb/makeQWebServerDeb.sh
@@ -7,18 +7,18 @@ CUR_DIR=`pwd`
 DOCKERIMAGEPATH="${CUR_DIR}/QWebServer-image*.tar.gz"
 mkdir -p QWebServer/tmp
 cp $DOCKERIMAGEPATH QWebServer/tmp/
+cp homunculusd.service QWebServer/tmp/
 
-VERSIONSTRING=$1
+VERSIONSTRING=2.0.5.5
 DEBCONTROLFILE="Package: QWebServer
 Version: $VERSIONSTRING
 Maintainer: Aqueti
 Architecture: amd64
-Depends: aquetidaemon, docker-ce
+Depends: docker-ce
 Description: The Aqueti SocketAPI Server"
 echo "$DEBCONTROLFILE" > QWebServer/DEBIAN/control
 
 cp ${CUR_DIR}/postinst QWebServer/DEBIAN/postinst
 
 dpkg-deb --build QWebServer
-
 rm -rf QWebServer/
