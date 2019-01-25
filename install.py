254533675d89baed1d557e36ab9cd9d1959c785e
Alex Stepenko <al.stepenko@gmail.com>
2019-01-25 14:19:21 -0500

    new install.py

--- install.py
+++ install.py
@@ -38,14 +38,14 @@ if "--help" in sys.argv:
 
 if "--cam" in sys.argv:
     cam_id = sys.argv[sys.argv.index("--cam") + 1]
+    cam_ip = '10.1.' + cam_id + '.'
+    start_ip = 1
     
     if cam_id == str(9) or cam_id == str(12):
-        cam_ip = '10.1.' + cam_id + '.'
-        start_ip = 1
         num_of_tegras = 9
-    else:
-        cam_ip = '10.1.' + cam_id + '.'
-        start_ip = 1       
+    elif cam_id == str(149):
+        num_of_tegras = 3
+    else:   
         num_of_tegras = 10
 else:
     print("cam isn't specified\n")
@@ -72,11 +72,6 @@ else:
 
     build = get_max(res)
 
-if "--run" in sys.argv:
-    isRun = True  #sys.argv[sys.argv.index("--run") + 1]
-else:
-    isRun = False
-
 try:
     page = urllib.request.urlopen(base_url + '/' + branch_name + '/' + str(build))
 except Exception:
@@ -95,28 +90,26 @@ os.system("mkdir -p " + folder_path)
 
 files = dict()
 for e in res:
-    if ".deb" in e.text or ".gz" in e.text:
+    if ".deb" in e.text:
         if "Daemon" in e.text:
             if "x86_64" in e.text:
-                if "debug" in e.text:
-                    files["daemon_x86_debug"] = e.text
+                if type == 'debug':
+                    if 'debug' not in e.text:
+                        continue
                 else:
-                    if '-application' in e.text:
-                        files["daemon_x86-app"] = e.text
-                    elif '-daemon' in e.text:
-                        files["daemon_x86-d"] = e.text
+                    if 'debug' in e.text:
+                        continue
+                
+                if '-application' in e.text:
+                    files["daemon_x86-app"] = e.text
+                elif '-daemon' in e.text:
+                    files["daemon_x86-d"] = e.text
             else:
                 files["daemon_aarch64"] = e.text
         elif "ACI" in e.text:
             files["aci"] = e.text
         elif "API" in e.text:
             files["api"] = e.text
-        elif "QView" in e.text:
-            files["qviewer"] = e.text
-        elif "QWebServer" in e.text:
-            files["qwebserver"] = e.text
-        elif "homunculus" in e.text:
-            files["homunculus"] = e.text
         elif "CalibrationTools" in e.text:
             files["ctools"] = e.text
 
@@ -147,22 +140,14 @@ os.system("sudo dpkg -r aquetidaemon-daemon")
 os.system("sudo dpkg -r aquetidaemon-application")
 os.system("sudo dpkg -r aquetiapi")
 os.system("sudo dpkg -r calibrationtools")
-        
-if type == "release":
-    if 'daemon_x86-app' in files.keys():        
-        os.system("sudo dpkg -i " + folder_path + files["daemon_x86-app"])
-        os.system("sudo dpkg -i " + folder_path + files["daemon_x86-d"])        
-else:
-    if 'daemon_x86_debug' in files.keys():
-        
-        os.system("sudo dpkg -i " + folder_path + files["daemon_x86_debug"])
+    
+if 'daemon_x86-app' in files.keys():        
+    os.system("sudo dpkg -i " + folder_path + files["daemon_x86-app"])
+    os.system("sudo dpkg -i " + folder_path + files["daemon_x86-d"])        
 
 if 'api' in files.keys():    
     os.system("sudo dpkg -i " + folder_path + files["api"])
 if 'ctools' in files.keys():        
     os.system("sudo dpkg -i " + folder_path + files["ctools"])
-
-if 'qwebserver' in files.keys():
-    pass
 
 print("***** Done *****")
