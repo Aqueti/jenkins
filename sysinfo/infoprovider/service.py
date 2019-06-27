70cc830319afa0ffe352ef58ab284bb4b710cb5b
alxstep <al.stepenko@gmail.com>
2019-06-27 08:31:16 -0400

    Update service.py

--- sysinfo/infoprovider/service.py
+++ sysinfo/infoprovider/service.py
@@ -1,17 +1,24 @@
 s = """
 [Unit]
 Description=SysInfo Service
+After=syslog.target
 
 [Service]
 Type=Simple
+User=mosaic
+Group=mosaic
 WorkingDirectory=/opt/infoprovider
 ExecStart=/usr/bin/python3 /opt/infoprovider/provider.py
+StandardOutput=syslog
+StandardError=syslog
 Restart=always
 RestartSec=30
 
+
 [Install]
-WantedBy=sysinit.target
+WantedBy=multi-user.target
+
 """
 
-with open("/etc/systemd/system/sysinfo.service", "w") as file:
+with open("/etc/systemd/system/infoprovider.service", "w") as file:
     file.write(s)
