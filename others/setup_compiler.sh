4cc211420705e17c850902f40ad110effc952609
Alex Stepenko <alstepenko@gmail.com>
2020-10-13 16:39:02 -0400

    reorg

---
+++ others/setup_compiler.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y \
+net-tools \
+git \
+build-essential \
+cmake-curses-gui  \
+g++ \
+openssh-server \
+doxygen \
+graphviz \
+libusb-1.0-0-dev \
+libz3-dev \
+libxrandr2 \
+libxrandr-dev \
+libxinerama-dev \
+libxcursor-dev \
+libxi-dev \
+libjpeg9-dev \
+libgl1-mesa-dev \
+libegl1-mesa-dev \
+libssl-dev \
+libsnappy-dev \
+libboost-all-dev \
+libglfw3-dev  \
+libglfw3 \
+libglew-dev \
+libgles2-mesa-dev \
+zlib1g-dev \
+mesa-utils \
+libavahi-common-dev \
+libavahi-client-dev \
+pyqt5-dev \
+qtbase5-dev \
+pyqt5-dev-tools \
+python3-pyqt5 \
+nvidia-driver-440 \
+nvidia-cuda-dev \
+nvidia-cuda-toolkit \
+swig3.0
